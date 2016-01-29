eeddb1891d8c7cdef44bfafdb93ad0d88b0435b1
Nicole Chiang <Nicole@Nicoles-MacBook-Pro.local>
2016-01-29 08:56:14 -0500

    adding css classes to stylize html elements in basic layout

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 console.log("I'm running");
+
+// 1-- MAKE LOGIC WORK --> SHUFFLE, DRAW, RECOGNIZE BUST, MAKE ACE WORK
+
+    // -- SHUFFLE
+
+    // -- DRAW
+
+    // -- RECOGNIZE BUST
+
+    // -- RECOGNIZE BLACKJACK
+
+    // -- FUNCTIONAL ACE
+
+// 2-- DO DEALER (look up rules-- when to continue drawing) 
+
+// 3-- BETTING (can't bet negative, can't bet more than you have)
+
+// 4-- BUTTON FUNCTIONALITY
+
+// 5-- HIDE ONE DEALER CARD
+
+    // -- ADD A CSS CLASS TO FIRST DEALER'S CARD?
+
+// 6-- WINS AND LOSSES COUNTER
